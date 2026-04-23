4835611d8e28aad6ffab41de6e3cd5a613beb0e1
Oliver Atherton <118294630+olivrrcode@users.noreply.github.com>
2026-04-23 21:56:02 +0100

    feat: add task-queue

--- src/consts/project-backends.js
+++ src/consts/project-backends.js
@@ -1,4 +1,3 @@
 export const PROJECT_BACKENDS = Object.freeze({
-  "task-queue":
-    "https://dashboard.render.com/web/srv-d7l8e0ojs32c73812i20/deploys/dep-d7l8e18js32c73812ih0",
+  "task-queue": "https://task-queue-backend.onrender.com",
 });
